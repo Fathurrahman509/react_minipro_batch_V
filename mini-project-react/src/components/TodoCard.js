20d9efe1fd017cbe70ffe84eb15831049c807fd5
Fathurrahman509 <rahman12ahm4n@gmail.com>
2022-05-05 21:00:20 +0700

    step five

---
+++ mini-project-react/src/components/TodoCard.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import './TodoCard.css'
+
+ class TodoCard extends Component {
+  render() {
+    return (
+      <div className='card-container'>
+          <div className='content-container'>
+              <h2>{this.props.todo.title}</h2>
+              <p>{this.props.todo.description}</p>
+          </div>
+          <button>Delete</button>
+
+      </div>
+    )
+  }
+}
+
+export default TodoCard
--- mini-project-react/src/components/TodoContainer.js
+++ mini-project-react/src/components/TodoContainer.js
@@ -1,12 +1,44 @@
 import React, { Component } from 'react'
 import './TodoContainer.css'
 import AddTodo from './AddTodo'
+import TodoCard from './TodoCard'
 
 class TodoContainer extends Component {
+  constructor() {
+    super();
+      this.state = {
+        todos: [
+          {
+            title: 'learning Front End',
+            description: 'learn html, javascript, css'
+          },
+          {
+            title: 'learning Back End',
+            description: 'learn mysql, nodejs, postman'
+          },
+          {
+            title: 'learning Framework',
+            description: 'learn angular.js, vue.js, react.js'
+          }
+        ]
+      }
+      this.updateTodos = this.updateTodos.bind(this)
+  }
+
+  updateTodos (todo) {
+    const newTodos = {...this.state.todos, todo}
+    this.setState({
+      todos: newTodos
+    })
+  }
+
   render() {
     return (
       <section>
-        <AddTodo />
+        <AddTodo updateTodos={this.updateTodos.todos}/>
+        {this.state.todos.map(todo => {
+          return <TodoCard todo={todo} key={todo.title}/>
+        })}
       </section>
     )
   }
